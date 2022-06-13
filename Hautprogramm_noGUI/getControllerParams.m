51af4c3333a81dc9df1985f20daef5d08a0ddfe2
Uni <Uni@172.22.253.115>
2022-06-13 23:25:07 +0200

    Funktion implementiert, die Reglerparameter abhängig von Totzeit(->Samplerateabhängig) bestimmt

---
+++ Hautprogramm_noGUI/getControllerParams.m
@@ -0,0 +1,12 @@
+function [ki, kp] = getControllerParams(axis, TD)
+    switch axis
+        case 'pan'
+            K = 1.1749;
+            T = 0.091;
+        case 'tilt'
+            K = 2.4;
+            T = 0.07;
+    end
+    kp = (0.9/K)*(T/TD);
+    ki = kp/(3.33*TD);
+end
